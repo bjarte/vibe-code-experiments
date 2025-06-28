0684189ac160283b6d232bb2a1083e7c36c50cb7
Bjarte Aune Olsen <bjarte@auneolsen.no>
2025-06-29 00:38:10 +0200

    Deleted unused datafile

--- bun-hono-zod-sanity-api/data/localizationData.ts
+++
@@ -1,9 +0,0 @@
-import type { LocalizationEntry } from '../models/localizationEntry'
-
-export const localizationData: Record<string, LocalizationEntry> = {
-  "mykey": {
-    "nb-NO": "Min nøkkel",
-    "nn-NO": "Mi nykjel",
-    "en-GB": "My key"
-  }
-}
