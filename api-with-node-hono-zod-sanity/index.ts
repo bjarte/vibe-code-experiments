ce324f12ffabff82629715f8d568ca368523ea1e
Bjarte Aune Olsen <bjarte@auneolsen.no>
2025-06-29 02:02:39 +0200

    rewrote main app to use hono for node instead of bun

--- api-with-node-hono-zod-sanity/index.ts
+++ api-with-node-hono-zod-sanity/index.ts
@@ -1,10 +1,11 @@
+import { serve } from '@hono/node-server'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { z } from 'zod'
 import type { LocalizationEntry } from './models/localizationEntry'
 import { DummyLocalizationRepository } from './repositories/localizationRepository'
 
-const app = new Hono()
+export const app = new Hono()
 const localizationRepo = new DummyLocalizationRepository()
 
 const idSchema = z.object({
@@ -12,18 +13,20 @@ const idSchema = z.object({
 })
 
 app.get('/api/:id', zValidator('param', idSchema), (context) => {
+
   const { id } = context.req.valid('param')
 
-  const translations: LocalizationEntry | undefined = localizationRepo.getById(id)
+  const translations: LocalizationEntry | undefined
+    = localizationRepo.getById(id)
 
   if (!translations) {
     return context.json({ error: 'ID not found' }, 404)
   }
 
   return context.json(translations)
+
 })
 
-export default {
-  port: 3000,
-  fetch: app.fetch,
-}
+serve(app, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
